f0af652b75afb23e0c8c634fb24805c4bd158411
the-true-kid <aaronweiss00@gmail.com>
2024-06-06 23:21:34 +0300

    added get all products endpoint

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const port = 3000;
 
 // Initialize passport configurations
-setupPassport();
+setupPassport(passport);
 
 // Middleware setup
 app.use(bodyParser.json());
@@ -25,11 +25,18 @@ app.use(passport.session());
 
 // Routes setup
 app.use('/api/users', require('./routes/userRoutes'));
-app.use('/api/products', require('./routes/productRoutes'))
-app.use('/api/cart', require('./routes/cartRoutes'))
+app.use('/api/products', require('./routes/productRoutes'));
+app.use('/api/cart', require('./routes/cartRoutes'));
+//app.use('/api/order', require('./routes/orderRoutes'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 app.listen(port, () => {
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,24 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const { getProductById, getProductsByCategory } = require('../queries/productQueries');
-
-
+const { getProductById, getProductsByCategory, getAllProducts } = require('../queries/productQueries');  
 
 router.get('/', async (req, res) => {
     const { category } = req.query;
-    if (!category) {
-        return res.status(400).json({ error: 'Category ID is required' });
-    }
-
     try {
-        const products = await getProductsByCategory(category);
+        let products;
+        if (category) {
+            products = await getProductsByCategory(category);
+        } else {
+            products = await getAllProducts();
+        }
         res.json(products);
     } catch (err) {
         console.error('Failed to get products', err);
         res.status(500).json({ error: 'Internal server error' });
     }
 });
-
 
 router.get('/:productId', async (req, res) => {
     try {
